1ef1b2d5e0c5067c17dfb1df89404fdea7b9cb33
Johannes Griss <jgriss8@yahoo.com>
2018-08-16 10:42:54 +0100

    Basic integer normalizer that simply multiplies the passed doubles with a constant. Currently mainly intended to be used for m/z values.

---
+++ src/test/java/org/spectra/cluster/normalizer/BasicIntegerNormalizerTest.java
@@ -0,0 +1,24 @@
+package org.spectra.cluster.normalizer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class BasicIntegerNormalizerTest {
+    @Test
+    public void testBasicNormalizer() {
+        double[] doublesToConvert = {0.1552, 100.2523, 5000.125, 299.99};
+        BasicIntegerNormalizer normalizer = new BasicIntegerNormalizer(BasicIntegerNormalizer.MZ_CONSTANT);
+
+        int[] convertedValues = normalizer.binDoubles(Arrays.stream(doublesToConvert).mapToObj(Double::new).collect(Collectors.toList()));
+
+        Assert.assertEquals(4, convertedValues.length);
+        Assert.assertEquals(15520, convertedValues[0]);
+        Assert.assertEquals(10025230, convertedValues[1]);
+        Assert.assertEquals(500012500, convertedValues[2]);
+        Assert.assertEquals(29999000, convertedValues[3]);
+
+    }
+}
