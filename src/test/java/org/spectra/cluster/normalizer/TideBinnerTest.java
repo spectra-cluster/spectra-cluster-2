bcdd6e65cf2b906d9ad23bfebdde3797bf2cac3e
Johannes Griss <jgriss8@yahoo.com>
2018-10-08 14:04:47 +0100

    Implemented binner as found in, for example, Tide

---
+++ src/test/java/org/spectra/cluster/normalizer/TideBinnerTest.java
@@ -0,0 +1,59 @@
+package org.spectra.cluster.normalizer;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.spectra.cluster.model.spectra.BinarySpectrum;
+import uk.ac.ebi.pride.tools.jmzreader.model.Spectrum;
+import uk.ac.ebi.pride.tools.mgf_parser.MgfFile;
+
+import java.io.File;
+import java.net.URI;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class TideBinnerTest {
+    private Spectrum testSpectrum;
+
+    @Before
+    public void setUp() throws Exception {
+        URI uri = Objects.requireNonNull(TideBinnerTest.class.getClassLoader().getResource("single-spectra.mgf")).toURI();
+        MgfFile mgfFile = new MgfFile(new File(uri));
+        Iterator<Spectrum> specIt = mgfFile.getSpectrumIterator();
+
+        if (!specIt.hasNext()) {
+            throw new Exception("Failed to load spectra");
+        }
+
+        testSpectrum = specIt.next();
+    }
+
+    @Test
+    public void testManualBinning() {
+        double[] doublesToBin = {0.01, 1.18, 1.19, 18.9};
+        List<Double> doubleList = Arrays.stream(doublesToBin).boxed().collect(Collectors.toList());
+        TideBinner binner = new TideBinner();
+
+        int[] indexes = binner.binDoubles(doubleList);
+
+        Assert.assertEquals(doublesToBin.length, indexes.length);
+        Assert.assertEquals(0, indexes[0]);
+        Assert.assertEquals(1, indexes[1]);
+        Assert.assertEquals(1, indexes[2]);
+        Assert.assertEquals(19, indexes[3]);
+    }
+
+    @Test
+    public void testBinFirstPeaks() {
+        List<Double> doubleList = new ArrayList<>(testSpectrum.getPeakList().keySet());
+        Collections.sort(doubleList);
+        SequestBinner binner = new SequestBinner();
+
+        int[] bins = binner.binDoubles(doubleList);
+
+        Assert.assertEquals(bins.length, doubleList.size());
+        Assert.assertEquals(126, bins[0]);
+        Assert.assertEquals(129, bins[1]);
+        Assert.assertEquals(130, bins[2]);
+    }
+}
